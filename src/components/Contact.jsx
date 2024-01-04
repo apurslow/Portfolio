5e0b932d115606baf5ba1e068b2bd6ba7dfdd69b
apurslow <aaronp26@uw.edu>
2024-01-04 14:47:12 -0800

    a

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -37,8 +37,8 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        process.env.EMAILJS_SERVICE_ID,
+        process.env.EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
           to_name: "Aaron",
@@ -46,7 +46,7 @@ const Contact = () => {
           to_email: "apurslow@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        process.env.EMAILJS_PUBLIC_KEY
       )
       .then(
         () => {

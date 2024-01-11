43fd6e7fdb6e245840fbc9629fe36ab3a851616d
apurslow <aaronp26@uw.edu>
2024-01-11 12:28:14 -0800

    changed earth model displayed under contact

--- src/components/canvas/index.js
+++ src/components/canvas/index.js
@@ -2,5 +2,6 @@ import EarthCanvas from "./Earth";
 import BallCanvas from "./Ball";
 import ComputersCanvas from "./Computers";
 import StarsCanvas from "./Stars";
+import EarthCanvas2 from "./Earth2";
 
-export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas };
+export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas,EarthCanvas2 };
